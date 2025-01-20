18be870915ef2b698cfdf4fb16ede4df4202e075
mbijanpour <mahanbijanpour@gmail.com>
2025-01-20 11:01:41 +0330

    project initiation and templates managements

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    # return HttpResponse("Home page ...")
+    return render(request, "home/index.html")
