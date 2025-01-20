fb57378fa14a86bbb73891b3e6e01fb222e22627
mbijanpour <mahanbijanpour@gmail.com>
2025-01-20 17:56:46 +0330

    adding widgetsto to the register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'john doe'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'johnDoe@gmail.com'}))
     # the wiget is used to hide the passwor
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    # the attrs is used to add a class to the input field
+    # this is for html/css classes hwich used to add a bootstrap class to the input field
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'your password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'confirm password'}))
