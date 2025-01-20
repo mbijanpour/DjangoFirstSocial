f5b8eec7a3369eaa94a6b718cc7f9125b048020f
mbijanpour <mahanbijanpour@gmail.com>
2025-01-20 18:47:53 +0330

    Refactor user registration form and view, add password confirmation and message handling

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,7 @@ class UserRegisterForm(forms.Form):
     # the wiget is used to hide the passwor
     # the attrs is used to add a class to the input field
     # this is for html/css classes hwich used to add a bootstrap class to the input field
-    password = forms.CharField(widget=forms.PasswordInput(
+    password1 = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'your password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'confirm password'}))
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 from .forms import UserRegisterForm
 
 
 class RegisterView(View):
+    # this is to avoid the reusability of the form in the functions if the form got changed
+    form_class = UserRegisterForm
+    template_name = 'accounts/register.html'
 
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, 'accounts/register.html', {'form': form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            password1 = cd['password1']
+            password2 = cd['password2']
+            if password1 == password2:
+                User.objects.create_user(
+                    username=cd['username'],
+                    email=cd['email'],
+                    password=password1)
+                messages.success(
+                    request, 'Account created successfully',
+                    extra_tags='alert alert-success')
+                return redirect('home:home')
+            else:
+                messages.error(
+                    request, 'Passwords do not match',
+                    extra_tags='alert alert-danger')
+        return render(request, self.template_name, {'form': form})
