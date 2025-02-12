4007734fc1d7a663e2017af1e5141b92556546cf
mbijanpour <mahanbijanpour@gmail.com>
2025-02-12 15:33:26 +0330

    Add search functionality to home page; implement SearchForm and update views and template

--- home/views.py
+++ home/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
+from .forms import *
 from posts.models import Post
 
 
 class HomeView(View):
+    class_form = SearchForm
+
     def get(self, request):
         posts = Post.objects.all()
-        return render(request, "home/index.html", {"posts": posts})
+        if request.GET.get('search'):
+            posts = posts.filter(body__contains=request.GET['search'])
+        return render(request, "home/index.html", {"posts": posts, "search_form": self.class_form})
 
     def post(self, request):
         return HttpResponse("POST request")
